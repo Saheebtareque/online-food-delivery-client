232295c47094688f51680a61bbf3046281356182
saheeb_tareque <saheebtareque97@gmail.com>
2021-11-04 15:13:40 +0600

    initialization done with firebase

---
+++ src/components/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD-bF7yS9g4QINyx0NqAhT3ztyemJRkaso",
+    authDomain: "online-food-delivery-d1ebd.firebaseapp.com",
+    projectId: "online-food-delivery-d1ebd",
+    storageBucket: "online-food-delivery-d1ebd.appspot.com",
+    messagingSenderId: "12476799809",
+    appId: "1:12476799809:web:02c8cc29f046efb9c1dfc0"
+  };
+
+  export default firebaseConfig;
